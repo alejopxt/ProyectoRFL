252299c9fcbf7c9f0f844fcc06f720c9f28c23a4
alejopxt <alejandromartinezortegon@gmail.com>
2025-04-06 17:42:38 -0500

    Primeras paginas Financiero Laboral

---
+++ src/app/financiero-laboral/gastos/editar-gastos/editar-gastos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gastos/editar',
+  standalone: false,
+  templateUrl: './editar-gastos.component.html',
+  styleUrl: './editar-gastos.component.scss'
+})
+export class EditarGastosComponent {
+
+}
---
+++ src/app/financiero-laboral/ingresos/editar-ingresos/editar-ingresos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ingresos/editar',
+  standalone: false,
+  templateUrl: './editar-ingresos.component.html',
+  styleUrl: './editar-ingresos.component.scss'
+})
+export class EditarIngresosComponent {
+
+}
