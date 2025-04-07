7bfcce4b97e133fed188eef6a7e5cfacc9914860
jhljhkjhkjhk <mauriciogomezh2004@gmail.com>
2025-04-07 17:04:11 -0500

    subido pa

--- src/app/reportes/reportes-routing.module.ts
+++ src/app/reportes/reportes-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GenerarComponent } from './pages/generar/generar.component';
+import { ConsultarComponent } from './pages/consultar/consultar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "generar", component: GenerarComponent},
+  {path: "consultar", component: ConsultarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/reportes/reportes.module.ts
+++ src/app/reportes/reportes.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReportesRoutingModule } from './reportes-routing.module';
+import { GenerarComponent } from './pages/generar/generar.component';
+import { SharedModule } from 'app/shared/shared.module';
+import { ConsultarComponent } from './pages/consultar/consultar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GenerarComponent,
+    ConsultarComponent
+  ],
   imports: [
     CommonModule,
-    ReportesRoutingModule
+    ReportesRoutingModule,
+    SharedModule
+  ],
+  exports: [
+    GenerarComponent,
+    ConsultarComponent
   ]
 })
 export class ReportesModule { }
